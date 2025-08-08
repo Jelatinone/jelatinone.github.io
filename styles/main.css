2ac8129fe8caf8e4b05c555d398dd253ead6e059
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2025-08-08 16:35:41 -0500

    Initial Commit

---
+++ styles/main.css
@@ -0,0 +1,8 @@
+/*
+    ~~~
+    AUTHOR: CODY WASHINGTON
+    CREATED: 8.8.2025
+    TITLE: A Portfolio: Cody Washington
+    DESCRIPTION: Static portoflio website, showcasing several projects, my experience, and relevant skills
+    ~~~
+*/
