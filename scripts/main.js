2a72de473f4249606d0335d9644edef3edf328d3
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2025-08-08 19:24:53 -0500

    Add Name & Details

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,64 @@ document.addEventListener('mousemove', e => {
   document.documentElement.style.setProperty('--x', `${e.clientX}px`);
   document.documentElement.style.setProperty('--y', `${e.clientY}px`);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const titleElement = document.getElementById('portfolio-title');
+  const titlePhrases = [
+    "Software Developer",
+    "Controls Engineer",
+    "Robotics Engineer",
+    "Creative Problem Solver",
+    "Author",
+    "Hobbyist Photographer"
+  ];
+
+  const typeStates = {
+    TYPE: 0,
+    DELETE: 1,
+    PAUSE: 2
+  };
+  const deleteSpeed = 150; 
+  const typeSpeed = 200;    
+  const pauseSpeed = 1500;  
+
+  let titlePhraseIndex = 0;
+  let titlePhraseState = typeStates.TYPE;
+  let currentPhrase = "";
+
+  function stepTitlePhrase() {
+    const titlePhrase = titlePhrases[titlePhraseIndex];
+
+    switch(titlePhraseState) {
+      case typeStates.TYPE:
+        currentPhrase = titlePhrase.substring(0, currentPhrase.length + 1);
+        titleElement.innerText = currentPhrase;
+        if(currentPhrase.length === titlePhrase.length) {
+          titlePhraseState = typeStates.PAUSE;
+          setTimeout(stepTitlePhrase, pauseSpeed);
+        } else {
+          setTimeout(stepTitlePhrase, typeSpeed);
+        }
+        break;
+
+      case typeStates.DELETE:
+        currentPhrase = titlePhrase.substring(0, currentPhrase.length - 1);
+        titleElement.innerText = currentPhrase;
+        if(currentPhrase.length === 0) {
+          titlePhraseState = typeStates.TYPE;
+          titlePhraseIndex = (titlePhraseIndex + 1) % titlePhrases.length;
+          setTimeout(stepTitlePhrase, typeSpeed);
+        } else {
+          setTimeout(stepTitlePhrase, deleteSpeed);
+        }
+        break;
+
+      case typeStates.PAUSE:
+        titlePhraseState = typeStates.DELETE;
+        setTimeout(stepTitlePhrase, deleteSpeed);
+        break;
+    }
+  }
+
+  stepTitlePhrase();
+});
